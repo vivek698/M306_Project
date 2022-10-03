9616907ae82aef12bff93ece87462209fffc8719
diego <fantinod@bzz.ch>
2022-10-03 13:43:40 +0200

    Funktioniert Nicht!!!

--- src/data/DataHandler.java
+++ src/data/DataHandler.java
@@ -1,6 +1,13 @@
 package data;
 
+import model.Messwerte;
+
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.util.TreeMap;
+import java.util.logging.FileHandler;
 
 public class DataHandler {
 
@@ -13,6 +20,27 @@ public class DataHandler {
 
     public static void readSDAT() {
 
+        try {
+            File file = new File("data/sdat/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml");
+
+            JAXBContext jaxbContext = JAXBContext.newInstance(Messwerte.class);
+
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+            unmarshaller.unmarshal(file);
+
+            Messwerte messwerte = (Messwerte) unmarshaller.unmarshal(file);
+
+            //System.out.println(messwerte.getRelativeEinspeisung());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        readSDAT();
     }
 
     public static void readESL() {
--- src/model/Messwerte.java
+++ src/model/Messwerte.java
@@ -1,5 +1,16 @@
 package model;
 
+
+import jdk.nashorn.internal.objects.annotations.Property;
+
+import javax.xml.bind.JAXB;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement()
 public class Messwerte {
 
     private String timestamp;
@@ -11,5 +22,45 @@ public class Messwerte {
 
     public Messwerte() {
 
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getRelativeEinspeisung() {
+        return relativeEinspeisung;
+    }
+
+    public void setRelativeEinspeisung(double relativeEinspeisung) {
+        this.relativeEinspeisung = relativeEinspeisung;
+    }
+
+    public double getAbsoluteEinspeisung() {
+        return absoluteEinspeisung;
+    }
+
+    public void setAbsoluteEinspeisung(double absoluteEinspeisung) {
+        this.absoluteEinspeisung = absoluteEinspeisung;
+    }
+
+    public double getRelativerBezug() {
+        return relativerBezug;
+    }
+
+    public void setRelativerBezug(double relativerBezug) {
+        this.relativerBezug = relativerBezug;
+    }
+
+    public double getAbsoluterBezug() {
+        return absoluterBezug;
+    }
+
+    public void setAbsoluterBezug(double absoluterBezug) {
+        this.absoluterBezug = absoluterBezug;
     }
 }
