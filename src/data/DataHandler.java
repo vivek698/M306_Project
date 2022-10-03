6fb8a37e387b998bc3a32270f920f4191cd38986
diego <fantinod@bzz.ch>
2022-10-03 11:20:06 +0200

    added DataHandler

---
+++ src/data/DataHandler.java
@@ -0,0 +1,16 @@
+package data;
+
+public class DataHandler {
+
+    public static void readSDAT() {
+
+    }
+
+    public static void readESL() {
+
+    }
+
+    public static void readJSON() {
+
+    }
+}
