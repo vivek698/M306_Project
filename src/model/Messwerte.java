bd966beaca94247cc807ca1bee589a8017ee943c
diego <fantinod@bzz.ch>
2022-10-03 11:54:38 +0200

    added Messwerte

---
+++ src/model/Messwerte.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Messwerte {
+
+    private int timestamp;
+    private double relativeEinspeisung;
+    private double absoluteEinspeisung;
+    private double relativerBezug;
+    private double absoluterBezug;
+
+
+    public Messwerte() {
+
+    }
+}
