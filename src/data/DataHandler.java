6b8dc184c757fe342849755cc6f0e5999341ca01
JBhaha <69484637+JBhaha@users.noreply.github.com>
2022-10-04 08:43:19 +0200

    DataHandler readSDAT

--- src/data/DataHandler.java
+++ src/data/DataHandler.java
@@ -1,10 +1,16 @@
 package data;
 
 import model.Messwerte;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.util.TreeMap;
 import java.util.logging.FileHandler;
@@ -21,17 +27,43 @@ public class DataHandler {
     public static void readSDAT() {
 
         try {
-            File file = new File("data/sdat/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml");
+            File file = new File(
+                    "data/sdat/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml");
 
-            JAXBContext jaxbContext = JAXBContext.newInstance(Messwerte.class);
+            DocumentBuilderFactory dbf
+                    = DocumentBuilderFactory.newInstance();
 
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
 
-            unmarshaller.unmarshal(file);
+            doc.getDocumentElement().normalize();
+            System.out.println(
+                    "Root element: "
+                            + doc.getDocumentElement().getNodeName());
 
-            Messwerte messwerte = (Messwerte) unmarshaller.unmarshal(file);
+            NodeList nodeList
+                    = doc.getElementsByTagName("rsm:Observation");
 
-            //System.out.println(messwerte.getRelativeEinspeisung());
+            for (int i = 0; i < nodeList.getLength(); ++i) {
+                Node node = nodeList.item(i);
+                System.out.println("\nNode Name :"
+                        + node.getNodeName());
+
+                if (node.getNodeType()
+                        == Node.ELEMENT_NODE) {
+                    Element tElement = (Element)node;
+                    System.out.println("Sequence: "
+                            + tElement
+                            .getElementsByTagName("rsm:Sequence")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Volume: "
+                            + tElement
+                            .getElementsByTagName("rsm:Volume")
+                            .item(0)
+                            .getTextContent());
+                }
+            }
 
 
         } catch (Exception e) {
