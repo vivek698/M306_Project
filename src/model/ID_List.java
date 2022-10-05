0dc87b9ebe86586270c889d42c931410d4481187
vviru <viruthiyelv@bzz.ch>
2022-10-05 09:14:07 +0200

    csv List finished

--- src/model/ID_List.java
+++ src/model/ID_List.java
@@ -2,26 +2,25 @@ package model;
 
 import data.DataHandler;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
 public class ID_List {
     List<Long> timestampList = DataHandler.getTimeStampList();
     TreeMap<Long, Messwerte> messwerteTreeMap  = DataHandler.getMap();
-    String id;
+    String sensorId;
     TreeMap<Long, Double> data = new TreeMap<>();
 
-    public ID_List(String id) {
-        this.id = id;
+    public ID_List(String sensorId) {
+        this.sensorId = sensorId;
 
 
-        if (id.equals("ID735")){
+        if (sensorId.equals("ID735")){
             for (Long timestamp:timestampList) {
                 data.put(timestamp, messwerteTreeMap.get(timestamp).getRelativeEinspeisung());
             }
 
-        } else if (id.equals("ID742")){
+        } else if (sensorId.equals("ID742")){
             for (Long timestamp:timestampList) {
                 data.put(timestamp, messwerteTreeMap.get(timestamp).getRelativerBezug());
             }
